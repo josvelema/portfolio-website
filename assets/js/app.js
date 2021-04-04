b1bb8f0f144fb7b311b2e3de0f024118656cd421
josvelema <volumensolutions@gmail.com>
2021-04-04 02:37:44 +0200

    git reset to a28f1f1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
 // portfolio Jos Velema 2021
+
+const source = document.getElementsByTagName('html')[0].innerHTML
+const clickPrint = document.getElementById('clickPrint')
+const domContainer = document.getElementById('domContainer')
+clickPrint.addEventListener("click",function printDOM() {
+    domContainer.innerText = source
+})
+
+domContainer.innerText = source
